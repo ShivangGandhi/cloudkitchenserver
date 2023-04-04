61679cb39e99427d5a1cdcb76a15ddd82dc60020
Shivangkumar Gandhi <sh966188@dal.ca>
2023-04-04 00:37:32 -0300

    Port changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 
-const port = 8080;
+const port = 3001;
 
 const app = express();
 dotenv.config()
